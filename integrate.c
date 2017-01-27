68b9f4e52925de881c8a6988bc42ebefddf2b291
Michael Kinsey <michael.kinsey@umontana.edu>
2017-01-26 17:33:13 -0700

    Add cache hack to integrate.c to get around false sharing

--- integrate.c
+++ integrate.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <omp.h>
 #define REQ_NUM 4
+#define PAD 8 // assume 64 byte L1 cache line size
 
 static long num_steps = 10000000;
 double step;
@@ -12,7 +13,7 @@ int main() {
     omp_set_num_threads(REQ_NUM);
     int actual_num_threads = 0;
 
-    double sums[REQ_NUM];
+    double sums[REQ_NUM][PAD];
     step = 1.0/(double) num_steps;
 
     double start_time = omp_get_wtime();
@@ -28,13 +29,13 @@ int main() {
 
         for (i=thread; i<num_steps; i+=num_threads) {
             x = (i + 0.5) * step;
-            sums[thread] += 4.0/(1.0 + x*x);
+            sums[thread][0] += 4.0/(1.0 + x*x);
         }
     }
 
     // combine data
     for(int i=0; i<actual_num_threads; i++) {
-        pi += sums[i] * step;
+        pi += sums[i][0] * step;
     }
 
     printf("Threads: %d\nResult: %f\nSteps: %d\n", 
