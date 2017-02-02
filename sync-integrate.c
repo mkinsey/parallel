f19a4bba525b3007be2b75903f439b3771631cf6
Michael Kinsey <michael.kinsey@umontana.edu>
2017-02-01 17:30:28 -0700

    Add versions of the integrate pi program

---
+++ sync-integrate.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <omp.h>
+#define REQ_NUM 4
+
+static long num_steps = 10000000;
+double step;
+
+int main() {
+    double pi, sum = 0.0;
+
+    // request 4
+    omp_set_num_threads(REQ_NUM);
+    int actual_num_threads = 0;
+
+    step = 1.0/(double) num_steps;
+
+    double start_time = omp_get_wtime();
+#pragma omp parallel
+    {
+        double x;
+        double thread_sum = 0.0;
+        int i;
+        int thread = omp_get_thread_num();
+        int num_threads = omp_get_num_threads();
+
+        // update num threads in shared memory
+        if(thread == 0) actual_num_threads = num_threads; 
+
+        for (i=thread; i<num_steps; i+=num_threads) {
+            x = (i + 0.5) * step;
+            thread_sum += 4.0/(1.0 + x*x);
+        }
+#pragma omp critical
+        sum += thread_sum;
+    }
+
+    pi = sum * step;
+
+    printf("Threads: %d\nResult: %f\nSteps: %d\n", 
+            actual_num_threads, pi, (int)num_steps);
+
+    double end_time = omp_get_wtime();
+    printf("Seconds elapsed: %.5f\n", end_time - start_time);
+}
