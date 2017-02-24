e20dc6f84cfe7fcfbb97e9594eb7992f18b64a74
Michael Kinsey <michael.kinsey@umontana.edu>
2017-02-24 15:13:08 -0700

    Update original matmul static w new args, cleanup

--- hw2/phase2/matmul_static.c
+++ hw2/phase2/matmul_static.c
@@ -8,7 +8,7 @@
 // will take place. One such case is when for
 // each row of A ( rows 0 <= i < n)
 //             sum(j=0 ... n-1) abs(a[i][j])  < 1.0    
-// Then the itteration will converge, assuming no roundoff or overflow.
+// Then the iteration will converge, assuming no roundoff or overflow.
 // Example
 // % ./matmul_static 4 10 5
 //
@@ -36,7 +36,7 @@
 void srand48(long int seedval);
 double drand48(void);
 
-#define N 100
+#define N 10000
 
 double 	a[N][N];// transformation matrix
 double 	b[N];   // transformation vector 
@@ -46,6 +46,7 @@ double  ts1[N];	// solution vector
 int main(int argc, char *argv[]) {
     int	n=4;	// problem size
     int	seed=10;// seed for srand48() / drand48()
+
     double  *t=ts;  // pointer to solution vector
     double  *t1=ts1;// pointer to next itteration of solution vector
     double	*ttemp;	// used to swap t1 and t at each itteration
@@ -53,23 +54,37 @@ int main(int argc, char *argv[]) {
     int	itt_max=5;// number of itterations to preform
     int	itt;	// current itteration
     int	i, j;   // indices into arrays
+    int nthreads; // number of threads to use
 
     double	sum;	// computes the inner products for A * t
     double 	error;  // max | t1[i] - t[i] |
     double 	errori; // | t1[i] - t[i] |
     char	ch;	// for error checking on command line args.
+    char   *filename; // specify output file
+    FILE *fp; //
+    filename = "static-out-original.txt"; // default file name
+    fp = fopen(filename, "w"); // open file
 
-    FILE *fp;
-    fp = fopen("../static-out.txt", "w");
 
-    if( argc == 4 ) {
+    // parse args
+    if( argc == 6 ) {
         if( (sscanf(argv[1],"%d %[^ /t]", &n, &ch) != 1) ||
             (sscanf(argv[2],"%d %[^ /t]", &seed, &ch) != 1) ||
-            (sscanf(argv[3],"%d %[^ /t]", &itt_max, &ch) != 1) ) {
-            fprintf(stderr," ERROR : useage: %s [ <n> <seed> <itt_max>]\n", argv[0]);
+            (sscanf(argv[3],"%d %[^ /t]", &itt_max, &ch) != 1) ||
+            (sscanf(argv[4],"%d %[^ /t]", &nthreads, &ch) != 1))
+        {
+            fprintf(stderr," ERROR : useage: %s [ <n> <seed> <itt_max> <n-threads> <file>]\n", argv[0]);
             return(1);
         }
-    } else if(argc != 1 ) {
+
+        // open and test file
+        fp = fopen(argv[5], "w");
+        if(fp == 0){
+            fprintf(stderr, "ERROR : could not open file: %s", argv[5]);
+            return(1);
+        }
+    }
+    else if(argc != 1 ) {
         fprintf(stderr," ERROR : useage: %s [ <n> <seed> <itt_max>]\n", argv[0]);
         return(1);
     }
@@ -77,6 +92,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr," ERROR :  n must be positive and <= %d.\n", N);
         return(1);
     }
+
 
 // Generate matrix a with | eigenvalues | < 1
     srand48((long int)seed);
@@ -89,33 +105,44 @@ int main(int argc, char *argv[]) {
         fprintf(fp, "\n");
     }
     fprintf(fp, "\n  b=\n");
-// Generate vector b 
+
+    // Generate vector b
     for(i=0; i< n; i++) {
         b[i] = 10.0 * drand48();
         fprintf(fp, "%10.6f ", b[i]);
     }
     fprintf(fp ,"\n");
-// Initialize t
+
+    // Initialize t to elements in b
     for(i=0; i< n; i++) {
         t[i] = b[i];
     }
 
     fprintf(fp, "\n  itt  error\n");
 
-    // multiplication
+    // dot product of a and t
     for(itt=0; itt<=itt_max; itt++) {
         error=0.0;
+        // column i in a
         for(i=0; i< n; i++) {
+
             sum = 0.0;
+            // row j in a, col j in t
             for(j=0; j< n; j++) {
                 sum += a[i][j] * t[j];
             }
-            t1[i] = sum + b[i];
+
+            {
+                t1[i] = sum + b[i];
+            }
+
             errori = fabs(t1[i]-t[i]);
             if(errori > error) {
                 error=errori;
             }
         }
+
+        // swap t and t1
         ttemp = t1;
         t1 = t;
         t = ttemp;
