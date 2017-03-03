8114e238f723c811b7fb4eb5b2e59b018d2927e7
Michael Kinsey <michael.kinsey@umontana.edu>
2017-03-02 19:57:15 -0700

    Finish pthread implementation of matmul static

--- hw2/phase2/pt_matmul_static.c
+++ hw2/phase2/pt_matmul_static.c
@@ -29,6 +29,9 @@
 //    4   2.832084e-02
 //    5   9.015857e-03
 //
+
+#define _XOPEN_SOURCE 600
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -38,59 +41,90 @@ void srand48(long int seedval);
 double drand48(void);
 
 #define N 10000
+#define ITT_MAX 10
 
+int	n=4;	// problem size
+int nthreads=4; // number of threads to use
 double 	a[N][N];// transformation matrix
 double 	b[N];   // transformation vector 
-double  ts[N];	// solution vector
-double  ts1[N];	// solution vector
+double  ts[ITT_MAX+2][N];	// solution vector. +1 for extra iteration +1 for filling 0 with b
 
-void *dotproduct(void* i){
-    int id = (int)i;
+int	nit=5;// number of iterations to preform
+double 	errori[ITT_MAX+1][N]; // | t1[i] - t[i] |
+FILE *fp; // file pointer
+pthread_barrier_t barrier;
 
-    printf("Hello %d\n", id);
-    pthread_exit(NULL);
+
+/*
+ * This function is called by each thread after it is initialized.
+ */
+void *dotproduct(void* arg){
+    int itt;
+    int j, i;
+    long id = (long)arg; // thread id
+    double	sum = 0;	// computes the inner products for A * t
+
+    // dot product of a and t
+    for(itt=0; itt<=nit; itt++) {
+
+        printf("Hello thread %li iteration %d\n", id, itt);
+        // column i in a
+        for(i=id; i< n; i+=nthreads) {
+
+            sum = 0.0;
+//             row j in a, col j in t
+            for(j=0; j< n; j++) {
+                sum += a[i][j] * ts[itt][j];
+            }
+
+            ts[itt+1][i] = sum + b[i];
+
+            errori[itt][i] = fabs(ts[itt+1][i]-ts[itt][i]);
+        }
+
+        // wait for all threads to finish this iteration
+        pthread_barrier_wait(&barrier);
+
+        if(id == 0){
+            // TODO find max error
+            double current_max = errori[itt][0];
+            for(i=1; i<nit; i++){
+                if(errori[itt][i] > current_max)
+                    current_max = errori[itt][i];
+            }
+            fprintf(fp, "%5d %14.6e\n", itt, current_max);
+        }
+
+    }
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-    int	n=4;	// problem size
     int	seed=10;// seed for srand48() / drand48()
 
-    double  *t=ts;  // pointer to solution vector
-    double  *t1=ts1;// pointer to next itteration of solution vector
-    double	*ttemp;	// used to swap t1 and t at each itteration
+//    double  *t=ts;  // pointer to solution vector
+//    double  *t1=ts1;// pointer to next itteration of solution vector
+//    double	*ttemp;	// used to swap t1 and t at each itteration
 
-    int	itt_max=5;// number of itterations to preform
-    int	itt;	// current itteration
-    int	i, j;   // indices into arrays
-    int nthreads=4; // number of threads to use
+    int	i, j;
+    long threadnum =0;
 
-    double	sum;	// computes the inner products for A * t
-    double 	error;  // max | t1[i] - t[i] |
-    double 	errori; // | t1[i] - t[i] |
     char	ch;	// for error checking on command line args.
     char   *filename; // specify output file
-    FILE *fp; //
-    filename = "static-out-original.txt"; // default file name
-    fp = fopen(filename, "w"); // open file
-
+    filename = "static-out-pthreads.txt"; // default file name
 
     // parse args
     if( argc == 6 ) {
         if( (sscanf(argv[1],"%d %[^ /t]", &n, &ch) != 1) ||
             (sscanf(argv[2],"%d %[^ /t]", &seed, &ch) != 1) ||
-            (sscanf(argv[3],"%d %[^ /t]", &itt_max, &ch) != 1) ||
+            (sscanf(argv[3],"%d %[^ /t]", &nit, &ch) != 1) ||
             (sscanf(argv[4],"%d %[^ /t]", &nthreads, &ch) != 1))
         {
             fprintf(stderr," ERROR : useage: %s [ <n> <seed> <itt_max> <n-threads> <file>]\n", argv[0]);
             return(1);
         }
+        filename = argv[5];
 
-        // open and test file
-        fp = fopen(argv[5], "w");
-        if(fp == 0){
-            fprintf(stderr, "ERROR : could not open file: %s", argv[5]);
-            return(1);
-        }
     }
     else if(argc != 1 ) {
         fprintf(stderr," ERROR : useage: %s [ <n> <seed> <itt_max>]\n", argv[0]);
@@ -100,9 +134,20 @@ int main(int argc, char *argv[]) {
         fprintf(stderr," ERROR :  n must be positive and <= %d.\n", N);
         return(1);
     }
+    if( nit<1 || nit > ITT_MAX){
+        fprintf(stderr," ERROR :  itt_max must be positive and <= %d.\n", ITT_MAX);
+        return(1);
+    }
+    // open and test file
+    fp = fopen(filename, "w");
+    if(fp == 0){
+        fprintf(stderr, "ERROR : could not open file: %s", argv[5]);
+        return(1);
+    }
 
+    pthread_t threads[nthreads]; // threads
 
-// Generate matrix a with | eigenvalues | < 1
+    // Generate matrix a with | eigenvalues | < 1
     srand48((long int)seed);
     fprintf(fp, "\n  a=\n");
     for(i=0; i< n; i++) {
@@ -123,49 +168,26 @@ int main(int argc, char *argv[]) {
 
     // Initialize t to elements in b
     for(i=0; i< n; i++) {
-        t[i] = b[i];
+        ts[0][i] = b[i];
     }
+    fprintf(fp, "\n  itt  error\n");
+
+    // init barrier
+    pthread_barrier_init(&barrier, NULL, nthreads);
 
     // set up pthreads
-    pthread_t threads[nthreads];
-    for(i=0; i<nthreads; i++){
-        pthread_create(&threads[i], NULL, dotproduct, (void*)i);
+    for(threadnum=0; threadnum<nthreads; threadnum++){
+        pthread_create(&threads[threadnum], NULL, dotproduct, (void*)threadnum);
     }
+
 
     // join threads
-    for (i=0; i<nthreads; i++){
-        pthread_join(threads[i], NULL);
+    for (threadnum=0; threadnum<nthreads; threadnum++){
+        pthread_join(threads[threadnum], NULL);
     }
 
-    // dot product of a and t
-    fprintf(fp, "\n  itt  error\n");
-    for(itt=0; itt<=itt_max; itt++) {
-        error=0.0;
-        // column i in a
-        for(i=0; i< n; i++) {
-
-            sum = 0.0;
-            // row j in a, col j in t
-            for(j=0; j< n; j++) {
-                sum += a[i][j] * t[j];
-            }
-
-            {
-                t1[i] = sum + b[i];
-            }
-
-            errori = fabs(t1[i]-t[i]);
-            if(errori > error) {
-                error=errori;
-            }
-        }
-
-        // swap t and t1
-        ttemp = t1;
-        t1 = t;
-        t = ttemp;
-        fprintf(fp, "%5d %14.6e\n", itt, error);
-    }
+    // cleanup
+    fclose(fp);
 
     return(0);
 }
